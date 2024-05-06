c4a7d34925c6825bb4594a95d37f9eaa60f6cacd
Muhammadumar <59314275+xmsi@users.noreply.github.com>
2024-05-06 13:53:27 +0500

    Update BlogNew.php

--- app/Models/BlogNew.php
+++ app/Models/BlogNew.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class BlogNew extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function getNormalizedDateAttribute(){
+        $date = $this->created_at->locale('is_IS')->format('d.m.Y');
+        if($this->posted_at){
+            $date = Carbon::createFromFormat('Y-m-d', $this->posted_at)->format('d.m.Y');
+        }
+
+        return $date;
+    }
 }
