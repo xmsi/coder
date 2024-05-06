21b8c197972dcc45ca71bc997f54f8464c0f72f3
Muhammadumar <59314275+xmsi@users.noreply.github.com>
2024-05-06 13:53:06 +0500

    Update BlogNew.php

--- app/Http/Resources/BlogNew.php
+++ app/Http/Resources/BlogNew.php
@@ -15,18 +15,12 @@ class BlogNew extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $date = $this->created_at->locale('is_IS')->format('d.m.Y');
-        if($this->posted_at){
-            $date = Carbon::createFromFormat('Y-m-d', $this->posted_at)->format('d.m.Y');
-        }
-
-
         return [
-            'title' => $this->title,
-            'description' => $this->description,
+            'title' => $this->{'title_'.$request->lang},
+            'description' => $this->{'description_'.$request->lang},
             'image' => url('storage') . '/' . $this->image,
             'link' => $this->link,
-            'date' => $date,
+            'date' => $this->normalized_date,
         ];
     }
 }
